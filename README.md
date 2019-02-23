1e9e16602698c598033f677cf9a393bc7fad0a3a
Banner <bbannercoe@gmail.com>
2019-02-23 16:19:24 +0630

    Rock ya! :octocat:

---
+++ README.md
@@ -0,0 +1,17 @@
+# ExpanderTextView
+
+Here **ExpanderTextView** come again!. It is just a TextView ,when text line count is more than 3 , it will shrink with ellipsize and read more text.
+When select the TextView, it will show whole original text.
+
+
+Here is the things for further improvements.
+- [X] MVP ExpanderTextView
+- [ ] can control line count from xml and kotlin
+- [ ] can change CONTINUE_TEXT (currently it's available only **Read more**).
+- [ ] can change CONTINUE_TEXT (currently it come with **Blue**).
+- [ ] a bit neat animation.
+
+
+ExpanderTextView is influence from 
+* [ReadMoreTextView](https://github.com/bravoborja/ReadMoreTextView)
+* [Spantastic text styling with Spans](https://medium.com/androiddevelopers/spantastic-text-styling-with-spans-17b0c16b4568)
---
+++ app/src/main/java/com/banner/expandertextview/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.banner.expandertextview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //tv_tester.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    }
+}
